474e7457961cf2e7d42bd16c359755b513979ef2
Samurai <davi.cruz1@estudante.ifb.edu.br>
2025-04-16 21:41:17 -0300

    third commit

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-def ola_mundo():
-    print(f'Olá Projeto Integrador 3')
+import requests
 
-    if __name__ == "__main__":
-        ola_mundo()
+def ola_mundo():
+ response = requests.get("https://api.github.com")
+ print (f"Olá, turma! API Status: {response}")
+
+if __name__ == "__main__":
+ ola_mundo()
